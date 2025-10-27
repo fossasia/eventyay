8bbcbd530bcfa17084d72ff4799456452896c916
Srivatsav Auswin <75414859+Sak1012@users.noreply.github.com>
2025-10-27 21:18:14 +0530

    fix(email): add missing templates and static files (#1121)
    * fix(email): add missing templates and static files
    
    * Convert tab to spaces for consistency (from @copilot)
    
    * Apply suggestion from @Copilot
    
    * Fixes
    
    ---------
    
    Co-authored-by: Norbert Preining <norbert@preining.info>
    Co-authored-by: Mario Behling <mb@mariobehling.de>

--- app/eventyay/helpers/jinja.py
+++ app/eventyay/helpers/jinja.py
@@ -1,9 +1,11 @@
 from django.urls import reverse
-from jinja2 import pass_context
+from django.utils.translation import gettext as django_gettext, ngettext
+from django.utils.formats import date_format
+from jinja2 import Environment, pass_context
 from jinja2.runtime import Context
+from eventyay.helpers.templatetags.thumb import thumb
 
 
-# Ref: https://github.com/niwinz/django-jinja/blob/master/django_jinja/builtins/extensions.py#L173
 @pass_context
 def url_for(context: Context, name: str, *args, query_string=None, **kwargs):
     try:
@@ -16,4 +18,41 @@ def url_for(context: Context, name: str, *args, query_string=None, **kwargs):
         except AttributeError:
             current_app = None
     base_url = reverse(name, args=args, kwargs=kwargs, current_app=current_app)
-    return '{}?{}'.format(base_url, query_string) if query_string else base_url
+    return f"{base_url}?{query_string}" if query_string else base_url
+
+
+def jinja_gettext(message, **kwargs):
+    text = django_gettext(message)
+    if kwargs:
+        try:
+            text = text % kwargs
+        except Exception:
+            pass
+    return text
+
+
+def format_date(value, format_str="SHORT_DATE_FORMAT"):
+    if not value:
+        return ""
+    try:
+        return date_format(value, format_str)
+    except Exception:
+        return str(value)
+
+
+def environment(**options):
+    env = Environment(**options, autoescape=True)
+
+    env.globals.update({
+        'url_for': url_for,
+        '_': jinja_gettext,
+        'gettext': jinja_gettext,
+        'ngettext': ngettext,
+    })
+
+    env.filters.update({
+        'thumb': thumb,
+        'format_date': format_date,
+    })
+
+    return env
