6fd7f353014b859452c2d2370fc7bfacc781a4c7
Arnav Angarkar <arnav.angarkar20@gmail.com>
2025-11-12 22:04:52 +0530

    Add pre_social_login hook to populate wikimedia_username field
    - Override pre_social_login() in CustomSocialAccountAdapter
    - Extract username from MediaWiki OAuth extra_data
    - Populate user.wikimedia_username for both new and existing users
    - Add comprehensive logging to track username extraction
    - Fixes issue : SSO Username Mapping

--- app/eventyay/plugins/socialauth/adapter.py
+++ app/eventyay/plugins/socialauth/adapter.py
@@ -35,3 +35,42 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def on_authentication_error(self, request, provider, error=None, exception=None, extra_context=None):
         logger.error('Error while authorizing with %s: %s - %s', provider, error, exception)
         raise ImmediateHttpResponse(HttpResponseRedirect(reverse('control:index')))
+
+    def pre_social_login(self, request, sociallogin):
+        """
+        Invoked after a user successfully authenticates via a social provider,
+        but before the login is fully processed.
+        
+        This is the ideal place to extract and populate the wikimedia_username
+        from the OAuth provider's extra_data.
+        
+        Fixes issue #1214: SSO Username Mapping
+        """
+        # Only process MediaWiki logins
+        if sociallogin.account.provider != 'mediawiki':
+            logger.debug(f'Skipping pre_social_login for provider: {sociallogin.account.provider}')
+            return
+
+        # Extract extra_data from the social account
+        extra_data = sociallogin.account.extra_data
+        logger.info(f'Processing MediaWiki OAuth login. extra_data keys: {list(extra_data.keys())}')
+
+        # Extract wikimedia username from extra_data
+        # MediaWiki provides 'username' field, fallback to 'realname' if not present
+        wikimedia_username = extra_data.get('username') or extra_data.get('realname') or ''
+        
+        if wikimedia_username:
+            logger.info(f'Extracted wikimedia_username: {wikimedia_username}')
+            
+            # Populate the user's wikimedia_username field
+            user = sociallogin.user
+            user.wikimedia_username = wikimedia_username
+            
+            # Save if user already exists (update case)
+            if user.pk:
+                user.save(update_fields=['wikimedia_username'])
+                logger.info(f'Updated existing user {user.email} with wikimedia_username: {wikimedia_username}')
+            else:
+                logger.info(f'Will create new user with wikimedia_username: {wikimedia_username}')
+        else:
+            logger.warning(f'Could not extract wikimedia_username from extra_data: {extra_data}')
