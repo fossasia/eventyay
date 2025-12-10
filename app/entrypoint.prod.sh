bb9f704d31c6fc3cef49664cc49f493d489efb01
Norbert Preining <norbert@preining.info>
2025-12-10 15:06:00 +0100

    Changes to the deployment setup

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -11,7 +11,8 @@ echo "============== postgresql is ready =================="
 # Since static.dist is mounted as shared volume, we need to move over
 # the files from static.dist.backup to static.dist
 echo "============== moving files from static.dist.backup to static.dist ==="
-mv /home/app/web/eventyay/static.dist.backup/* /home/app/web/eventyay/static.dist/
+# mind the final dash after backup/ so that only the content is transfered!!!
+rync -avz --delete /home/app/web/eventyay/static.dist.backup/ /home/app/web/eventyay/static.dist
 #echo "============== compiling translation messages ================"
 #python manage.py compilemessages
 #echo "============== compiling JS translation messages ================"
