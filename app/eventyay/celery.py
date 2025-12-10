05fe7e0fcf0a83261a94d800043e4a942366fdef
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2025-12-10 19:46:16 +0700

    Update `DJANGO_SETTINGS_MODULE` to new value in entire code base

--- app/eventyay/celery.py
+++ app/eventyay/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventyay.config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventyay.config.next_settings')
 
 from django.conf import settings
 
