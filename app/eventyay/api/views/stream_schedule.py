f3ffa0c631069acf37ff79a386ac7a9dfc95c1cd
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2026-01-07 01:11:22 +0530

    Add StreamSchedule CRUD API and current stream endpoint

---
+++ app/eventyay/api/views/stream_schedule.py
@@ -0,0 +1,89 @@
+from asgiref.sync import async_to_sync
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework import viewsets
+
+from eventyay.api.documentation import build_search_docs
+from eventyay.api.mixins import PretalxViewSetMixin
+from eventyay.api.serializers.stream_schedule import StreamScheduleSerializer
+from eventyay.base.models.room import Room
+from eventyay.base.models.stream_schedule import StreamSchedule
+from eventyay.base.services.event import notify_event_change
+from eventyay.base.services.room import broadcast_stream_change
+
+
+@extend_schema_view(
+    list=extend_schema(
+        summary='List Stream Schedules', parameters=[build_search_docs('title')]
+    ),
+    retrieve=extend_schema(summary='Show Stream Schedule'),
+    create=extend_schema(
+        summary='Create Stream Schedule',
+        request=StreamScheduleSerializer,
+        responses={201: StreamScheduleSerializer},
+    ),
+    update=extend_schema(
+        summary='Update Stream Schedule',
+        request=StreamScheduleSerializer,
+        responses={200: StreamScheduleSerializer},
+    ),
+    partial_update=extend_schema(
+        summary='Update Stream Schedule (Partial Update)',
+        request=StreamScheduleSerializer,
+        responses={200: StreamScheduleSerializer},
+    ),
+    destroy=extend_schema(summary='Delete Stream Schedule'),
+)
+class StreamScheduleViewSet(PretalxViewSetMixin, viewsets.ModelViewSet):
+    queryset = StreamSchedule.objects.none()
+    serializer_class = StreamScheduleSerializer
+    endpoint = 'stream_schedules'
+    search_fields = ('title',)
+
+    def get_queryset(self):
+        room_id = self.kwargs.get('room_pk')
+        if room_id:
+            return StreamSchedule.objects.filter(
+                room_id=room_id, room__event=self.event
+            )
+        return StreamSchedule.objects.filter(room__event=self.event)
+
+    def perform_create(self, serializer):
+        room_id = self.kwargs.get('room_pk')
+        if room_id:
+            room = Room.objects.get(pk=room_id, event=self.event)
+            instance = serializer.save(room=room)
+        else:
+            instance = serializer.save()
+
+        current_stream = instance.room.get_current_stream()
+        if current_stream and current_stream.pk == instance.pk:
+            async_to_sync(broadcast_stream_change)(
+                instance.room.pk, instance, reload=True
+            )
+
+        async_to_sync(notify_event_change)(self.event.id)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        current_stream = instance.room.get_current_stream()
+        if current_stream and current_stream.pk == instance.pk:
+            async_to_sync(broadcast_stream_change)(
+                instance.room.pk, instance, reload=True
+            )
+
+        async_to_sync(notify_event_change)(self.event.id)
+
+    def perform_destroy(self, instance):
+        room_id = instance.room.pk
+        was_current = (
+            instance.room.get_current_stream()
+            and instance.room.get_current_stream().pk == instance.pk
+        )
+        instance.delete()
+
+        if was_current:
+            new_current = Room.objects.get(pk=room_id).get_current_stream()
+            async_to_sync(broadcast_stream_change)(room_id, new_current, reload=True)
+
+        async_to_sync(notify_event_change)(self.event.id)
+
