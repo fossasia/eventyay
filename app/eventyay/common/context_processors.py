4f0255760603b24d8ae4c8f2ee38ce85cb82ff89
Sak1012 <1012srivatsavas@gmail.com>
2026-01-06 23:37:53 +0530

    chore(language): Resolve User Interface language selection and add dashboard dropdown
    - Fix the language selection in account settings
    - Add language dropdown to the dashboard
    - Add english formaters for english uk, canada, us and australia.

--- app/eventyay/common/context_processors.py
+++ app/eventyay/common/context_processors.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.http import Http404, HttpRequest
 from django.urls import resolve
 from django.utils import translation
+from django.utils.translation import gettext_lazy as _
 from django_scopes import get_scope
 
 from eventyay.base.models.settings import GlobalSettings
@@ -41,6 +42,23 @@ def locale_context(request):
     AVAILABLE_CALENDAR_LOCALES = tuple(
         f.name.removesuffix('.global.min.js') for f in cal_static_dir.rglob('*.global.min.js')
     )
+    languages = sorted(
+        settings.LANGUAGES,
+        key=lambda l: (
+            0 if l[0] in settings.LANGUAGES_OFFICIAL else (1 if l[0] not in settings.LANGUAGES_INCUBATING else 2),
+            str(l[1]),
+        ),
+    )
+    language_options = []
+    for code, name in languages:
+        if code in settings.LANGUAGES_OFFICIAL:
+            label = name
+        elif code not in settings.LANGUAGES_INCUBATING:
+            label = _('{} (inofficial translation)').format(name)
+        else:
+            label = _('{} (translation in progress)').format(name)
+        language_options.append({'code': code, 'label': label})
+
     context = {
         'js_date_format': get_javascript_format('DATE_INPUT_FORMATS'),
         'js_datetime_format': get_javascript_format('DATETIME_INPUT_FORMATS'),
@@ -50,10 +68,15 @@ def locale_context(request):
         'DAY_MONTH_DATE_FORMAT': get_day_month_date_format(),
         'rtl': getattr(request, 'LANGUAGE_CODE', 'en') in settings.LANGUAGES_BIDI,
         'AVAILABLE_CALENDAR_LOCALES': AVAILABLE_CALENDAR_LOCALES,
+        'language_options': language_options,
     }
 
     lang = translation.get_language()
-    context['html_locale'] = translation.get_language_info(lang).get('public_code', lang)
+    try:
+        lang_info = translation.get_language_info(lang)
+        context['html_locale'] = lang_info.get('public_code', lang)
+    except KeyError:
+        context['html_locale'] = lang
     return context
 
 
