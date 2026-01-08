4454ea1d910b915b22d06784061c8f8f66f51636
prayag78 <dasprayag1234@gmail.com>
2026-01-08 15:47:18 +0530

    remove legacy talks landing page and intro text

--- app/eventyay/orga/views/event.py
+++ app/eventyay/orga/views/event.py
@@ -111,15 +111,8 @@ class EventLive(EventSettingsPermission, TemplateView):
         if not self.request.event.cfp.text or len(str(self.request.event.cfp.text)) < 50:
             warnings.append(
                 {
-                    'text': _('The CfP doesn’t have a full text yet.'),
+                    'text': _("The CfP doesn't have a full text yet."),
                     'url': self.request.event.cfp.urls.text,
-                }
-            )
-        if not self.request.event.landing_page_text or len(str(self.request.event.landing_page_text)) < 50:
-            warnings.append(
-                {
-                    'text': _('The event doesn’t have a landing page text yet.'),
-                    'url': self.request.event.orga_urls.settings,
                 }
             )
         # TODO: test that mails can be sent
