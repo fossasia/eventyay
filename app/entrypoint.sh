9b8a31e2a39a7bd563b8a554f341321bba599276
Abhijeet212004 <abhijeetbhosale540@gmail.com>
2026-01-15 01:52:25 +0530

    Fix: Remove database flush and update env vars in entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,13 +2,12 @@
 
 echo "Waiting for postgres..."
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+while ! nc -z ${EVY_POSTGRES_HOST:-$POSTGRES_HOST} ${EVY_POSTGRES_PORT:-$POSTGRES_PORT}; do
   sleep 0.1
 done
 
 echo "PostgreSQL started"
 
-python manage.py flush --no-input
 python manage.py migrate
 
 exec "$@"
