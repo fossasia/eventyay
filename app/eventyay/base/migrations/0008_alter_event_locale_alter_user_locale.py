b15c7be3be7870a03ba0d16150824636cbac9373
Arnav Angarkar <arnav.angarkar20@gmail.com>
2026-01-21 14:56:55 +0530

    Update migration 0008 to depend on 0007 from custom field PR

--- app/eventyay/base/migrations/0008_alter_event_locale_alter_user_locale.py
+++ app/eventyay/base/migrations/0008_alter_event_locale_alter_user_locale.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0006_remove_landing_page_text'),
+        ('base', '0007_make_custom_field_label_optional'),
     ]
 
     operations = [
