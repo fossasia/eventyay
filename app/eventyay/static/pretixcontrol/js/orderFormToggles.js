a8f58e21eb3b7baba6f521f87e2023cfdc141af1
ViRUS-0-0 <mrvanwariomg@gmail.com>
2026-01-25 15:20:23 +0530

    improve csrf token retrieval by first checking for a csrf input field

--- app/eventyay/static/pretixcontrol/js/orderFormToggles.js
+++ app/eventyay/static/pretixcontrol/js/orderFormToggles.js
@@ -131,7 +131,7 @@ function initOrderFormToggles() {
             
             try {
                 // Get CSRF token
-                const csrfToken = getCookie('pretix_csrftoken') || getCookie('csrftoken');
+                const csrfToken = document.querySelector('input[name=csrfmiddlewaretoken]')?.value || getCookie('pretix_csrftoken') || getCookie('csrftoken');
                 if (!csrfToken) {
                     throw new Error('CSRF token not found');
                 }
