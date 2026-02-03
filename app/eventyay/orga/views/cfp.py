abd787baedaaacd6d69be23ab98c1ea62da20eb0
ViRUS-0-0 <mrvanwariomg@gmail.com>
2026-02-04 01:20:25 +0530

    fixed the fallback logic

--- app/eventyay/orga/views/cfp.py
+++ app/eventyay/orga/views/cfp.py
@@ -172,10 +172,8 @@ class CfPForms(EventPermissionRequired, TemplateView):
             
             data = []
             for q in ordered_questions:
-                # Fallback if annotation fails
-                if getattr(q, 'answer_count', 0) == 0:
+                if not hasattr(q, 'answer_count'):
                     q.answer_count = q.answers.count()
-                    
                 data.append({
                     'question': q,
                     'field': sform[f'question_{q.pk}']
