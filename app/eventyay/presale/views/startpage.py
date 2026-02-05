4e17cd096a6aaf92f30b1dedf9c9f3bb12cb0570
SupRaKoshti <koshtisahil02@gmail.com>
2026-02-05 11:27:18 +0530

    Comment out event query logic in startpage.py
    Comment out event query logic for featured, upcoming, and past events.

--- app/eventyay/presale/views/startpage.py
+++ app/eventyay/presale/views/startpage.py
@@ -40,19 +40,19 @@ class StartPageView(TemplateView):
                 .order_by('date_from')
             )
         return ctx
-            qs = Event.objects.select_related('organizer').filter(live=True)
-            today = timezone.localdate()
-            ctx['featured_events'] = (
-                qs.filter(startpage_featured=True, testmode=False, date_to__date__gte=today)
-                .order_by('date_from')
-            )
-            ctx['upcoming_events'] = (
-                qs.filter(startpage_visible=True, date_to__date__gte=today)
-                .filter(models.Q(startpage_featured=False) | models.Q(testmode=True))
-                .order_by('date_from')
-            )
-            ctx['past_events'] = (
-                qs.filter(startpage_visible=True, date_to__date__lt=today)
-                .order_by('-date_from')
-            )
-        return ctx
+        #     qs = Event.objects.select_related('organizer').filter(live=True)
+        #     today = timezone.localdate()
+        #     ctx['featured_events'] = (
+        #         qs.filter(startpage_featured=True, testmode=False, date_to__date__gte=today)
+        #         .order_by('date_from')
+        #     )
+        #     ctx['upcoming_events'] = (
+        #         qs.filter(startpage_visible=True, date_to__date__gte=today)
+        #         .filter(models.Q(startpage_featured=False) | models.Q(testmode=True))
+        #         .order_by('date_from')
+        #     )
+        #     ctx['past_events'] = (
+        #         qs.filter(startpage_visible=True, date_to__date__lt=today)
+        #         .order_by('-date_from')
+        #     )
+        # return ctx
