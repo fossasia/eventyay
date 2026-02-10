5c9a96c5ec34296e9e8d1b2fbee2bd59ae6eb288
Aqil Ahmad <aqahmad.bscs23seecs@seecs.edu.pk>
2026-02-10 14:39:57 +0500

    Add admin menu item and fix text styling (#2305)
    * add admin menu item and fix text styling
    
    Co-authored-by: Srivatsav Auswin <75414859+Sak1012@users.noreply.github.com>
    
    * make color red on active admin session
    
    * rslv ai comments
    
    * add margin
    
    * fix(startpage): add missing endif
    
    ---------
    
    Co-authored-by: Srivatsav Auswin <75414859+Sak1012@users.noreply.github.com>
    Co-authored-by: Sak1012 <1012srivatsavas@gmail.com>

--- app/eventyay/presale/views/startpage.py
+++ app/eventyay/presale/views/startpage.py
@@ -8,6 +8,7 @@ from django.utils import timezone
 
 from eventyay.base.models import Event
 from eventyay.base.settings import GlobalSettingsObject
+from eventyay.common.permissions import is_admin_mode_active
 
 
 class StartPageView(TemplateView):
@@ -15,6 +16,7 @@ class StartPageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
+        ctx['staff_session'] = is_admin_mode_active(self.request)
         settings_obj = GlobalSettingsObject().settings
         header_image = settings_obj.get('startpage_header_image', as_type=str, default='')
         if header_image.startswith('file://'):
