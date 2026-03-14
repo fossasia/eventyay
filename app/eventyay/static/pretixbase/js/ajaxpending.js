7d30db2f650d3d1c405a9a59ecaa26c099a8269f
OsauravO <saurav1hasyo@gmail.com>
2026-03-14 12:05:17 +0530

    refactor: replace jQuery with Fetch API

--- app/eventyay/static/pretixbase/js/ajaxpending.js
+++ app/eventyay/static/pretixbase/js/ajaxpending.js
@@ -1,10 +1,16 @@
 var check = function () {
-    $.getJSON(location.href + '&ajax=1', function (data, status) {
-        if (data.redirect) {
-            location.href = data.redirect;
-        } else {
+    fetch(location.href + '&ajax=1')
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
+            if (data.redirect) {
+                location.href = data.redirect;
+            } else {
+                window.setTimeout(check, 500);
+            }
+        })
+        .catch(function (err) {
+            console.error('ajaxpending: poll failed', err);
             window.setTimeout(check, 500);
-        }
-    });
-}
+        });
+};
 window.setTimeout(check, 500);
