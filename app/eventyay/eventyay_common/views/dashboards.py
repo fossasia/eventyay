c625fb203602002b3695150bbf55fcec301fae0f
Saksham Sirohi <sakshamsirohi27@gmail.com>
2026-03-17 11:22:56 +0530

    Retrieve subevent and permissions in context data
    Added logic to retrieve subevent and user permissions in get_context_data.

--- app/eventyay/eventyay_common/views/dashboards.py
+++ app/eventyay/eventyay_common/views/dashboards.py
@@ -212,6 +212,7 @@ class EventIndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         request = self.request
+        
         # Get subevent and permissions
         subevent = get_subevent(request)
         permissions = self._get_user_permissions()
