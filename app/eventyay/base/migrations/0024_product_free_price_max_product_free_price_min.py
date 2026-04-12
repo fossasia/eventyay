fcb47b99d9c7abb54b785bf9fddf3707789fbd13
Rachit Makwana <rachitmakwana15@gmail.com>
2026-04-12 17:23:26 +0530

    Fix dependency reference in migration file

--- app/eventyay/base/migrations/0024_product_free_price_max_product_free_price_min.py
+++ app/eventyay/base/migrations/0024_product_free_price_max_product_free_price_min.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0023_product_allow_user_variation_change.py'),
+        ('base', '0023_product_allow_user_variation_change'),
     ]
 
     operations = [
