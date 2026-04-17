11cbac53f7156036c3c3c7b2875cf456862efb12
Rachit Makwana <rachitmakwana15@gmail.com>
2026-04-17 07:17:43 +0530

    Update migration dependencies for product free price

--- app/eventyay/base/migrations/0025_product_free_price_max_product_free_price_min.py
+++ app/eventyay/base/migrations/0025_product_free_price_max_product_free_price_min.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0023_product_allow_user_variation_change'),
+        ('base', '0024_remove_event_featured_sessions_text'),
     ]
 
     operations = [
