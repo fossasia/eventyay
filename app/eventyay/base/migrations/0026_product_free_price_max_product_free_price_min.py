797a566d761134f5dea5bc5f0a72d3f90b05f7fb
Rachit Makwana <rachitmakwana15@gmail.com>
2026-05-06 19:16:58 +0530

    Migration File Error

--- app/eventyay/base/migrations/0026_product_free_price_max_product_free_price_min.py
+++ app/eventyay/base/migrations/0026_product_free_price_max_product_free_price_min.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0024_remove_event_featured_sessions_text'),
+        ('base', '0025_resource_kind_talkquestion_import_key_and_more'),
     ]
 
     operations = [
