9cdae6dd0d524907a4b2df2b29c640085d58a6dc
Sarafaraj Nasardi <sarafarajnasardi786@gmail.com>
2026-06-15 21:59:34 +0530

    Fix multiple languages widget test

--- app/eventyay/orga/forms/widgets.py
+++ app/eventyay/orga/forms/widgets.py
@@ -1,9 +1,5 @@
-from django.conf import settings
 from django.forms import RadioSelect
 
 
 class HeaderSelect(RadioSelect):
     option_template_name = 'orga/widgets/header_option.html'
-
-
-
--- app/tests/stable/test_multiple_languages_widget.py
+++ app/tests/stable/test_multiple_languages_widget.py
@@ -1,39 +1,30 @@
-import pytest
 from bs4 import BeautifulSoup
 
 from eventyay.control.forms import MultipleLanguagesWidget
 
 
-@pytest.mark.django_db
 def test_multiple_languages_widget_rendering():
     widget = MultipleLanguagesWidget()
-    
-    # Dummy choices
-    choices = [
+    widget.choices = [
         ('xyz', 'Xyz Language'),
         ('abc', 'Abc Language'),
         ('en', 'English'),
     ]
-    
-    html = widget.render('test_locales', ['en', 'xyz'], attrs={'id': 'id_test_locales'}, choices=choices)
-    
-    # Assert grid markup is present
+
+    html = widget.render('test_locales', ['en', 'xyz'], attrs={'id': 'id_test_locales'})
+
     assert 'language-grid-widget' in html
-    
+
     soup = BeautifulSoup(html, 'html.parser')
-    
-    # Assert checkboxes have correct name and value
+
     checkboxes = soup.find_all('input', type='checkbox', attrs={'name': 'test_locales'})
     assert len(checkboxes) == 3
-    
+
     values = [cb['value'] for cb in checkboxes]
     assert 'en' in values
     assert 'abc' in values
     assert 'xyz' in values
-    
-    # Assert choices are sorted as expected
-    # The widget sorts by (official, incubating, other), then by name
-    # 'en' is official, so it should be first. 'abc' and 'xyz' are "other" and should be sorted alphabetically by name
+
     assert values[0] == 'en'
     assert values[1] == 'abc'
     assert values[2] == 'xyz'
