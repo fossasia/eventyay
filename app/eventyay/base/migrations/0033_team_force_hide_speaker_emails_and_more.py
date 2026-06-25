ece2d91247eabb53cdefe58ba36e7f3d714d83b0
Rachit Makwana <rachitmakwana15@gmail.com>
2026-06-25 22:16:58 +0530

    Update and rename 0032_team_force_hide_speaker_emails_and_more.py to 0033_team_force_hide_speaker_emails_and_more.py

--- app/eventyay/base/migrations/0033_team_force_hide_speaker_emails_and_more.py
+++ app/eventyay/base/migrations/0033_team_force_hide_speaker_emails_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0031_migrate_meta_noindex'),
+        ('base', '0032_submission_etherpad_url'),
     ]
 
     operations = [
