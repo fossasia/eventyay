63e5c93801664ac5a887adf08037c0361686c666
Rachit Makwana <rachitmakwana15@gmail.com>
2026-06-26 14:50:10 +0530

    Update and rename 0033_team_force_hide_speaker_emails_and_more.py to 0034_team_force_hide_speaker_emails_and_more.py

--- app/eventyay/base/migrations/0034_team_force_hide_speaker_emails_and_more.py
+++ app/eventyay/base/migrations/0034_team_force_hide_speaker_emails_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0032_submission_etherpad_url'),
+        ('base', '0033_alter_event_private_testmode_default'),
     ]
 
     operations = [
