be7ae839068ce8208f05a5f734042b1867818eb3
Rachit Makwana <rachitmakwana15@gmail.com>
2026-06-29 21:43:44 +0530

    Update and rename 0035_team_force_hide_speaker_emails_and_more.py to 0036_team_force_hide_speaker_emails_and_more.py

--- app/eventyay/base/migrations/0036_team_force_hide_speaker_emails_and_more.py
+++ app/eventyay/base/migrations/0036_team_force_hide_speaker_emails_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0034_migrate_native_stream_schedules'),
+        ('base', '0035_user_is_spam'),
     ]
 
     operations = [
