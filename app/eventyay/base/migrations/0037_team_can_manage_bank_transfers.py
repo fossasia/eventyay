20e32bd10434eb11d94907260f3037a13023e645
Rachit Makwana <rachitmakwana15@gmail.com>
2026-07-07 18:14:49 +0530

    Create migration for team bank transfer management

--- app/eventyay/base/migrations/0037_team_can_manage_bank_transfers.py
+++ app/eventyay/base/migrations/0037_team_can_manage_bank_transfers.py
@@ -9,7 +9,7 @@ def grant_bank_transfer_to_order_editors(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0035_user_is_spam'),
+        ('base', '0036_orderposition_search_trigram_indexes'),
     ]
 
     operations = [
