c34cd8b982794bd85ccd897b48a26bb897fcd829
Aqil-Ahmad <aaqilahmadd@gmail.com>
2026-07-08 13:06:47 +0500

    migration

--- app/eventyay/base/migrations/0037_team_exhibition_review_permissions.py
+++ app/eventyay/base/migrations/0037_team_exhibition_review_permissions.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0035_user_is_spam'),
+        ('base', '0036_orderposition_search_trigram_indexes'),
     ]
 
     operations = [
