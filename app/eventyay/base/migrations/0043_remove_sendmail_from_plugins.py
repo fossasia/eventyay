69362870e87f57e9b09d9b68894e8681081bae70
Rachit Makwana <rachitmakwana15@gmail.com>
2026-07-10 14:25:58 +0530

    Remove sendmail from plugins migration

--- app/eventyay/base/migrations/0043_remove_sendmail_from_plugins.py
+++ app/eventyay/base/migrations/0043_remove_sendmail_from_plugins.py
@@ -13,7 +13,7 @@ def remove_sendmail_from_plugins(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0041_make_event_email_optional'),
+        ('base', '0042_team_exhibition_review_permissions'),
     ]
 
     operations = [
