d65a72e013c6265f0ec91668b511198ef87ca2d3
Rachit Makwana <rachitmakwana15@gmail.com>
2026-07-24 14:38:59 +0530

    fix(mails): fix tab switch validation in tickets message centre

--- app/eventyay/static/pretixcontrol/js/ui/tabs.js
+++ app/eventyay/static/pretixcontrol/js/ui/tabs.js
@@ -8,7 +8,8 @@ $(function () {
         $form.addClass("tab-content");
 
         var i = 0;
-        var preselect = null;
+        var error_preselect = null;
+        var hash_preselect = null;
         var validity_error = false;
         $form.children("fieldset").each(function () {
             var $fieldset = $(this);
@@ -23,28 +24,29 @@ $(function () {
             if ($fieldset.find(".has-error, .alert-danger").length > 0) {
                 $tablink.append(" ");
                 $tablink.append($("<span>").addClass("fa fa-warning text-danger"));
-                if (preselect === null) {
-                    preselect = i;
+                if (error_preselect === null) {
+                    error_preselect = i;
                 }
             }
             $fieldset.find("input, select, textarea").on("invalid", function () {
                 if ($tablink.find(".fa-warning").length === 0) {
                     $tablink.append(" ");
                     $tablink.append($("<span>").addClass("fa fa-warning text-danger"));
-                    if (!validity_error) {
-                        validity_error = true;
-                        $tablink.click();
-                    }
+                }
+                if (!validity_error) {
+                    validity_error = true;
+                    $tablink.click();
                 }
             });
             $fieldset.find("legend").remove();
             $fieldset.addClass("tab-pane").attr("id", tid);
-            if (location.hash && ($fieldset.find(location.hash).length || location.hash === "#" + tid + "-open") && preselect === null) {
-                preselect = i;
+            if (location.hash && ($fieldset.find(location.hash).length || location.hash === "#" + tid + "-open") && hash_preselect === null) {
+                hash_preselect = i;
             }
             i++;
         });
-        $tabs.find("a").get(preselect != null ? preselect : 0).click();
+        var preselect = error_preselect !== null ? error_preselect : (hash_preselect !== null ? hash_preselect : 0);
+        $tabs.find("a").get(preselect).click();
         $tabs.find("a").on('shown.bs.tab', function (e) {
             history.replaceState(null, null, e.target.getAttribute("href") + "-open");
         });
