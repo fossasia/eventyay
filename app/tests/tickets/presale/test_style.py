b8bc78bd04f7e4fe333152922b66fcc96ba13144
Prayag <dasprayag1234@gmail.com>
2026-08-01 09:06:50 +0530

    bug fix

--- app/tests/tickets/presale/test_style.py
+++ app/tests/tickets/presale/test_style.py
@@ -2,11 +2,15 @@ import datetime
 import os.path
 
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.urls import reverse
 from django_scopes import scopes_disabled
 
 from eventyay.base.models import Event, Organizer
-from eventyay.presale.style import regenerate_organizer_css
+from eventyay.base.settings import validate_event_settings, validate_organizer_settings
+from eventyay.eventyay_common.forms.event import EventCommonSettingsForm
+from eventyay.presale.style import compile_scss, regenerate_css, regenerate_organizer_css
 
 
 class StyleTest(TestCase):
@@ -51,7 +55,6 @@ class StyleTest(TestCase):
             assert '#33c33c' not in c.read()
 
     def test_event_generate_css_primary_font(self):
-        from eventyay.presale.style import regenerate_css
         self.event.settings.primary_font = 'Georgia'
         regenerate_css.apply(args=(self.event.pk,))
         self.event.settings.flush()
@@ -71,10 +74,6 @@ class StyleTest(TestCase):
             assert 'Georgia' in content
 
     def test_validate_event_settings_primary_font(self):
-        from django.core.exceptions import ValidationError
-
-        from eventyay.base.settings import validate_event_settings
-
         # Valid system font
         validate_event_settings(self.event, {'primary_font': 'Georgia'})
 
@@ -83,10 +82,6 @@ class StyleTest(TestCase):
             validate_event_settings(self.event, {'primary_font': 'NonExistentFont'})
 
     def test_validate_organizer_settings_primary_font(self):
-        from django.core.exceptions import ValidationError
-
-        from eventyay.base.settings import validate_organizer_settings
-
         # Valid system font
         validate_organizer_settings(self.orga, {'primary_font': 'Georgia'})
 
@@ -95,8 +90,6 @@ class StyleTest(TestCase):
             validate_organizer_settings(self.orga, {'primary_font': 'NonExistentFont'})
 
     def test_event_font_form_inheritance(self):
-        from eventyay.eventyay_common.forms.event import EventCommonSettingsForm
-
         # 1. Set organizer font to Georgia
         self.orga.settings.primary_font = 'Georgia'
 
@@ -120,7 +113,6 @@ class StyleTest(TestCase):
         self.assertEqual(self.event.settings.get('primary_font'), 'Georgia')
 
     def test_event_css_view_with_font(self):
-        from django.urls import reverse
         self.event.settings.primary_font = 'Georgia'
         self.event.settings.primary_color = '#123456'
         response = self.client.get(
@@ -136,58 +128,60 @@ class StyleTest(TestCase):
         self.assertIn('--color-primary: #123456', content)
 
     def test_organizer_compile_scss_emits_header_background_color(self):
-        from eventyay.presale.style import compile_scss
-        self.orga.settings.header_background_color = '#aabbcc'
+        self.orga.settings.header_background_color = '#a1b2c3'
         css, _ = compile_scss(self.orga)
-        assert '--color-header-background: #aabbcc' in css
+        assert '--color-header-background' in css
+        assert '#a1b2c3' in css
 
     def test_organizer_compile_scss_emits_primary_color_as_css_var(self):
-        from eventyay.presale.style import compile_scss
-        self.orga.settings.primary_color = '#aa1122'
+        self.orga.settings.primary_color = '#a1b2c3'
         css, _ = compile_scss(self.orga)
-        assert '#aa1122' in css
-        assert '--color-primary: #aa1122' in css
+        assert '--color-primary' in css
+        assert '#a1b2c3' in css
 
     def test_organizer_compile_scss_emits_theme_colors_as_css_vars(self):
-        from eventyay.presale.style import compile_scss
-        self.orga.settings.theme_color_success = '#009900'
-        self.orga.settings.theme_color_danger = '#cc0000'
-        self.orga.settings.theme_color_background = '#eeeeee'
+        self.orga.settings.theme_color_success = '#129901'
+        self.orga.settings.theme_color_danger = '#c10001'
+        self.orga.settings.theme_color_background = '#e1e2e3'
         css, _ = compile_scss(self.orga)
-        assert '--color-success: #009900' in css
-        assert '--color-danger: #cc0000' in css
-        assert '--color-bg: #eeeeee' in css
+        assert '--color-success' in css
+        assert '#129901' in css
+        assert '--color-danger' in css
+        assert '#c10001' in css
+        assert '--color-bg' in css
+        assert '#e1e2e3' in css
 
     def test_organizer_compile_scss_emits_header_text_color(self):
-        from eventyay.presale.style import compile_scss
-        self.orga.settings.header_text_color = '#111111'
+        self.orga.settings.header_text_color = '#121314'
         css, _ = compile_scss(self.orga)
-        assert '--color-header-text: #111111' in css
+        assert '--color-header-text' in css
+        assert '#121314' in css
 
     def test_organizer_compile_scss_emits_navigation_text_color(self):
-        from eventyay.presale.style import compile_scss
-        self.orga.settings.navigation_text_color = '#222222'
+        self.orga.settings.navigation_text_color = '#232425'
         css, _ = compile_scss(self.orga)
-        assert '--color-header-navigation: #222222' in css
+        assert '--color-header-navigation' in css
+        assert '#232425' in css
 
     def test_organizer_compile_scss_emits_menu_text_scroll_over_color(self):
-        from eventyay.presale.style import compile_scss
-        self.orga.settings.menu_text_scroll_over_color = '#333333'
+        self.orga.settings.menu_text_scroll_over_color = '#343536'
         css, _ = compile_scss(self.orga)
-        assert '--color-header-navigation-hover: #333333' in css
+        assert '--color-header-navigation-hover' in css
+        assert '#343536' in css
 
     def test_organizer_compile_scss_emits_video_colors(self):
-        from eventyay.presale.style import compile_scss
-        self.orga.settings.video_navigation_background_color = '#444444'
-        self.orga.settings.video_sidebar_text_color = '#555555'
-        self.orga.settings.video_sidebar_hover_color = '#666666'
+        self.orga.settings.video_navigation_background_color = '#454647'
+        self.orga.settings.video_sidebar_text_color = '#565758'
+        self.orga.settings.video_sidebar_hover_color = '#676869'
         css, _ = compile_scss(self.orga)
-        assert '--color-video-nav-background: #444444' in css
-        assert '--color-video-sidebar-text: #555555' in css
-        assert '--color-video-sidebar-hover: #666666' in css
+        assert '--color-video-nav-background' in css
+        assert '#454647' in css
+        assert '--color-video-sidebar-text' in css
+        assert '#565758' in css
+        assert '--color-video-sidebar-hover' in css
+        assert '#676869' in css
 
     def test_organizer_compile_scss_no_custom_props_when_unset(self):
-        from eventyay.presale.style import compile_scss
         css, _ = compile_scss(self.orga)
         assert '--color-header-background' not in css
         assert '--color-header-text' not in css
@@ -195,51 +189,58 @@ class StyleTest(TestCase):
         assert '--color-header-navigation-hover' not in css
 
     def test_organizer_regenerate_css_applies_header_colors_to_organizer_file(self):
-        self.orga.settings.header_background_color = '#abcdef'
-        self.orga.settings.navigation_text_color = '#fedcba'
+        self.orga.settings.header_background_color = '#a1b2c3'
+        self.orga.settings.navigation_text_color = '#d1e2f3'
         regenerate_organizer_css.apply(args=(self.orga.pk,))
         self.orga.settings.flush()
         assert self.orga.settings.presale_css_file
         with open(os.path.join(settings.MEDIA_ROOT, self.orga.settings.presale_css_file)) as c:
             content = c.read()
-            assert '--color-header-background: #abcdef' in content
-            assert '--color-header-navigation: #fedcba' in content
+            assert '--color-header-background' in content
+            assert '#a1b2c3' in content
+            assert '--color-header-navigation' in content
+            assert '#d1e2f3' in content
 
     def test_event_inherits_header_colors_from_organizer(self):
-        self.orga.settings.header_background_color = '#112233'
-        self.orga.settings.navigation_text_color = '#445566'
+        self.orga.settings.header_background_color = '#a1b2c3'
+        self.orga.settings.navigation_text_color = '#d1e2f3'
         regenerate_organizer_css.apply(args=(self.orga.pk,))
+        regenerate_css.apply(args=(self.event.pk,))
+        self.event.refresh_from_db()
         self.event.settings.flush()
-        assert self.event.settings.presale_css_file
-        with open(os.path.join(settings.MEDIA_ROOT, self.event.settings.presale_css_file)) as c:
+        self.orga.settings.flush()
+        assert self.event.settings.presale_css_file or self.orga.settings.presale_css_file
+        css_file = self.event.settings.presale_css_file or self.orga.settings.presale_css_file
+        with open(os.path.join(settings.MEDIA_ROOT, css_file)) as c:
             content = c.read()
-            assert '--color-header-background: #112233' in content
-            assert '--color-header-navigation: #445566' in content
+            assert '--color-header-background' in content
+            assert '#a1b2c3' in content
+            assert '--color-header-navigation' in content
+            assert '#d1e2f3' in content
 
     def test_event_own_header_color_overrides_organizer(self):
-        from eventyay.presale.style import regenerate_css
-        self.orga.settings.header_background_color = '#000000'
-        self.event.settings.header_background_color = '#ffffff'
+        self.orga.settings.header_background_color = '#a1b2c3'
+        self.event.settings.header_background_color = '#d1e2f3'
         regenerate_css.apply(args=(self.event.pk,))
+        self.event.refresh_from_db()
         self.event.settings.flush()
         assert self.event.settings.presale_css_file
         with open(os.path.join(settings.MEDIA_ROOT, self.event.settings.presale_css_file)) as c:
             content = c.read()
-            assert '--color-header-background: #ffffff' in content
-            assert '--color-header-background: #000000' not in content
+            assert '--color-header-background' in content
+            assert '#d1e2f3' in content
+            assert '#a1b2c3' not in content
 
     def test_existing_scss_variables_still_emitted(self):
-        from eventyay.presale.style import compile_scss
         self.orga.settings.primary_color = '#33c33c'
-        self.orga.settings.theme_color_success = '#00ff00'
-        self.orga.settings.theme_color_danger = '#ff0000'
+        self.orga.settings.theme_color_success = '#009901'
+        self.orga.settings.theme_color_danger = '#ff0001'
         css, _ = compile_scss(self.orga)
         assert '#33c33c' in css
-        assert '#00ff00' in css
-        assert '#ff0000' in css
+        assert '#009901' in css
+        assert '#ff0001' in css
 
     def test_event_css_view_with_font_target_orga(self):
-        from django.urls import reverse
         self.event.settings.primary_font = 'Georgia'
         self.event.settings.primary_color = '#123456'
         response = self.client.get(
