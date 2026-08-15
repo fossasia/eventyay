6d85e866587582f2420f9b6f80e0aca789aeac73
Rachit Makwana <rachitmakwana15@gmail.com>
2026-08-15 12:07:53 +0530

    Update migration dependencies for Gmail OAuth credential

--- app/eventyay/base/migrations/0054_gmailoauthcredential.py
+++ app/eventyay/base/migrations/0054_gmailoauthcredential.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0052_admission_validity_fields'),
+        ('base', '0053_jitsiserver'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
