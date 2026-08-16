a477deabc69c7a0fe46bb6a37e9939f309af83af
anuragverma025 <anuragverma9613@gmail.com>
2026-08-17 02:03:47 +0530

    fix: resolve migration conflict for 0054

--- app/eventyay/base/migrations/0055_alter_product_default_price_and_more.py
+++ app/eventyay/base/migrations/0055_alter_product_default_price_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0053_jitsiserver'),
+        ('base', '0054_team_teamshifts_permissions'),
     ]
 
     operations = [
