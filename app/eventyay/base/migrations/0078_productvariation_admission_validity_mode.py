b78c6ec7755acd66bcce3702c5a0daf8d1a98363
Jagriti Jha <im.jagriti.jha@gmail.com>
2026-09-22 14:09:49 +0530

    fix: resolve multiple leaf nodes by renaming migration to 0078 and updating dependency

--- app/eventyay/base/migrations/0078_productvariation_admission_validity_mode.py
+++ app/eventyay/base/migrations/0078_productvariation_admission_validity_mode.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0076_default_speaker_questions'),
+        ('base', '0077_admin_message_center'),
     ]
 
     operations = [
